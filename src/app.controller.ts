881c97d8512549cce27fbca627203711f0254787
LucasDNiguti <niguti005@gmail.com>
2025-05-14 16:58:52 -0300

    feat: adiciona entidade Tarefa com campos id, titulo, descricao e status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Patch } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { TaskService } from './app.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.taskService.create(createTaskDto);
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.taskService.update(+id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.taskService.remove(+id);
   }
 }
