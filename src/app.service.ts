d7b6d6e149bc584f59342882e83164f1103b4870
LucasDNiguti <niguti005@gmail.com>
2025-05-14 17:01:35 -0300

    feat: implementa camada de serviço com regras de negócio para tarefas

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class TaskService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
+
+  create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.taskRepository.create(createTaskDto);
+    return this.taskRepository.save(task);
+  }
+
+  findAll(): Promise<Task[]> {
+    return this.taskRepository.find();
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto): Promise<any> {
+    return this.taskRepository.update(id, updateTaskDto);
+  }
+
+  remove(id: number): Promise<any> {
+    return this.taskRepository.delete(id);
   }
 }
